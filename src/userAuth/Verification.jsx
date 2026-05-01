66dc60b271330b5a1bbc9741fbc771a784955e4c
SuuSeej <262122906+jei-csan@users.noreply.github.com>
2026-05-01 20:42:38 +0800

    Add missing dependencies to useEffect hook in Verification.jsx

--- src/userAuth/Verification.jsx
+++ src/userAuth/Verification.jsx
@@ -93,7 +93,7 @@ const VerificationPage = () => {
       }
     };
     if (email && sourceFlow === "signup") sendOTP();
-  }, [email, sourceFlow]);
+  }, [email, sourceFlow, username, password]);
 
   useEffect(() => {
     if (!isTimerRunning || timer === 0) return;
